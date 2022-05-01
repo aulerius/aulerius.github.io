3a3ad66b4ca55751de9144aaad4a166b94bcdba0
aulerius <51526300+aulerius@users.noreply.github.com>
2022-05-01 03:18:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -41,9 +41,11 @@ function draw() {
   x = x + xspeed*deltaTime*0.05;
   y = y + yspeed*deltaTime*0.05;
   
-  if (x+logo_width>=windowWidth || x<=0){xspeed=-xspeed}
+  if (x+logo_width>=windowWidth){xspeed=-logo_width*logo_speed}
+  else if (x<=0){xspeed=logo_width*logo_speed}
 
-  if (y+logo_height>=windowHeight || y<=0){yspeed=-yspeed}
+  if (y+logo_height>=windowHeight){yspeed=-logo_width*logo_speed}
+  else if (y<=0){yspeed=logo_width*logo_speed}
   
 }
 
@@ -85,3 +87,29 @@ function loadedImage(img){
   updatelogoHeight();
 }
 
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("question_mark");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
